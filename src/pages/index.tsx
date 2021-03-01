f39e7db2a343206563ef4e4391886efd314f08db
Amirhoosein khademi <amirhossein.khademi@rdsysco.com>
2021-03-01 21:48:17 +0330

    TEST: Fix style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { ValetCard } from "containers";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-2 gap-x-32 py-44 px-80 bg-gray-100 fixed inset-0">
+    <div className="grid grid-cols-2 gap-x-32 py-44 px-60 bg-gray-100 fixed inset-0">
       <div />
       <ValetCard />
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  purge: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/containers/**/*.{js,ts,jsx,tsx}",
-    "./src/icons/**/*.{js,ts,jsx,tsx}",
-  ],
+  // purge: [
+  //   "./src/pages/**/*.{js,ts,jsx,tsx}",
+  //   "./src/components/**/*.{js,ts,jsx,tsx}",
+  //   "./src/containers/**/*.{js,ts,jsx,tsx}",
+  //   "./src/icons/**/*.{js,ts,jsx,tsx}",
+  // ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
